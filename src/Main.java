152d24588ad0504631674536f7e4df231491d0b1
AndreyTerechenko <Dronka97@yandex.ru>
2024-04-14 21:52:48 +0300

    repost3

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         Book book1 = new Book(1623, "Гамлет", author1);
         Author author2 = new Author("Джордж", "Мартин");
         Book book2 = new Book(1996, "Игра престолов", author2);
+        book2.setYear(1997);
         System.out.println(book1.getBookTitle() + " произведение " + author1.getName() + " " + author1.getLastName() +
                 " " + book1.getYear() + " года");
         System.out.println(book2.getBookTitle() + " произведение " + author2.getName() + " " + author2.getLastName() +
