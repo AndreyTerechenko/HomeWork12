0af5742de9c2c2b3cf218bc0e645fbec16f55b30
AndreyTerechenko <Dronka97@yandex.ru>
2024-04-14 20:19:15 +0300

    homeWork

---
+++ src/Book.java
@@ -0,0 +1,37 @@
+public class Book {
+    private int year;
+    private String bookTitle;
+    private Author author;
+
+    public Book(int year, String bookTitle, Author author) {
+        this.year = year;
+        this.bookTitle = bookTitle;
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Уильман", "Шекспир");
+        Book book1 = new Book(1623, "Гамлет", author1);
+        Author author2 = new Author("Джордж", "Мартин");
+        Book book2 = new Book(1996, "Игра престолов", author2);
+        System.out.println(book1.getBookTitle() + " произведение " + author1.getName() + " " + author1.getLastName() +
+                " " + book1.getYear() + " года");
+        System.out.println(book2.getBookTitle() + " произведение " + author2.getName() + " " + author2.getLastName() +
+                " " + book2.getYear() + " года");
+
+
+
     }
 }
